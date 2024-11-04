a2d2557d5d2ed87bf133be9e4908adc92c1a0ec5
pavel20063 <pavliktol1997@gmail.com>
2024-11-04 09:50:36 +0200

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TravelAgencyT.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
